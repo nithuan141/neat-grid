66a2c9b112ec03f0451b9edd6263faac423d2a00
Nithesh AN <nithuan141@gmail.com>
2019-01-31 19:51:13 +0530

    Custom component as a column and pagination

--- src/index.js
+++ src/index.js
@@ -1,44 +1,131 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const NeatGrid = (props) => {
+class NeatGrid extends React.PureComponent {
+   
+    constructor(props){
+        super(props);
+        this.state={
+            pageNo : 1
+        };
+        this.onNext = this.onNext.bind(this);
+        this.onPrev = this.onPrev.bind(this);
+    }
+
+    render(){
+        let paginationElement;
+        
+        let bodyData = this.props.bodyData.slice((this.state.pageNo - 1) * this.props.dataPerPage, this.state.pageNo * this.props.dataPerPage);
+
+        if(this.props.hasPagination){
+            let fromData = ((this.state.pageNo - 1) * this.props.dataPerPage);
+            paginationElement = <Pager onNext = {this.onNext} onPrev = {this.onPrev} 
+                                    fromData = { fromData + 1 } 
+                                    dataCount = { fromData + bodyData.length } />
+        }
+
         return (
-            <table>
-                <thead>
-                   <TableRow>
-                   {
-                       props.headerData.map((item)=>{
-                           return <TableHeader>{item}</TableHeader>
-                       })
-                   }
-                   </TableRow>
-                </thead>
-                <tbody>
+            <div className = {this.props.cssClass} id = {this.props.gridId}>
+                            {paginationElement }
+                <table >
+                    <thead>
+                    <TableRow>
                     {
-                        props.bodyData.map((list)=>{
-                            return <TableRow>
-                                {
-                                    list.map((item)=>{
-                                        return <TableData>{item}</TableData>
-                                    })
-                                }
-                             </TableRow>
+                        this.props.headerData.map((item)=>{
+                            return <TableHeader>{item}</TableHeader>
                         })
                     }
-                </tbody>
-            </table>
+                    </TableRow>
+                    </thead>
+                    <tbody>
+                        {
+                            bodyData.map((list)=>{
+                                return <TableRow>
+                                    {
+                                        list.map((item)=>{
+                                            return <TableData item={item}></TableData>
+                                        })
+                                    }
+                                </TableRow>
+                            })
+                        }
+                    </tbody>
+                </table>
+            </div>
         );
+    }
+
+    onNext = () =>{
+        if(this.state.pageNo * this.props.dataPerPage < this.props.bodyData.length){
+            this.setState({
+                pageNo: this.state.pageNo + 1
+            })
+        }
+    }
+
+    onPrev = () =>{
+        if(this.state.pageNo > 1){
+            this.setState({
+                pageNo: this.state.pageNo - 1
+            })
+        }
+    }
 };
 
+NeatGrid.propTypes = {
+    headerData: PropTypes.array,
+    bodyData: PropTypes.array,
+    cssClass: PropTypes.string,
+    gridId : PropTypes.string,
+    hasPagination: PropTypes.bool,
+    dataPerPage: PropTypes.number
+}
+
+/**
+ * table row component (tr)
+ */
 const TableRow = React.memo(function(props){
     return <tr className = {props.rowClass}>{props.children}</tr>
 });
 
+/**
+ * table data component (td)
+ */
 const TableData = React.memo(function(props){
-    return <td className = {props.dataClass}>{props.children}</td>
+    return (<td className = {props.dataClass}>
+        {getComponent(props.item)}
+    </td>);
 });
 
+/**
+ * table heder component (th)
+ */
 const TableHeader = React.memo(function(props){
     return <th className = {props.headerClass}>{props.children}</th>
 });
+
+const Pager = React.memo(function(props) {
+    return <React.Fragment>
+            <button onClick = {()=>{props.onPrev()}}>Prev</button>
+            <spa>{`Showing data ${props.fromData} to ${props.dataCount}`} </spa>
+            <button onClick = {()=>{props.onNext()}}>Next</button>
+    </React.Fragment>;
+});
+
+/**
+ * return the component based on the given type.
+ */
+const getComponent = function(item){
+    switch (item.type) {
+        case "input":
+            return <input type='text' className = {item.cssClass} value= {item.value} onChange = {item.onChange}/>;
+        case "label":
+            return <lable className = {item.cssClass} > {item.value}</lable>;
+        case "custom":
+            return item.value
+        default:
+            return <span>{item.value}</span>;
+    }
+}
 
 export default NeatGrid;
